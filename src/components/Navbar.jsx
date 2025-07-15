85538fb69698b9ea2ea7ecaa08c5df62e9d82edf
Mushaf Hafeez <mushafhafeez1122@gmail.com>
2025-07-15 11:03:17 +0500

    add navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,11 @@
+import React from "react";
+
+const Navbar = () => {
+  return (
+    <nav className="p-3 md:p-9">
+      <img src="/images/nav-logo.svg" alt="logo" />
+    </nav>
+  );
+};
+
+export default Navbar;
